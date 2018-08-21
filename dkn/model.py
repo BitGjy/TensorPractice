fcf83f547296525b5094c713d97db36ad9a0df95
BitGjy <79336927@qq.com>
2018-08-21 10:48:35 +0800

    project

---
+++ dkn/model.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+class DeepKN:
+
+    def __init__(self, *args, **kwargs):
+        
+        s
---
+++ seq2seq/model.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+class Seq2Seq:
+
+    def __init__(self, *args, **kwargs):
+        
+        
